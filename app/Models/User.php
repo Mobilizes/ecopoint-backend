c2ba51384eb1790aff80c757f8b5d3c477685606
Mobilizes <fernando142311@gmail.com>
2025-06-14 21:49:57 +0700

    fix: middleware fix and me returns token

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function currentAccessToken()
+    {
+        return request()->attributes->get('accessToken');
+    }
 }
